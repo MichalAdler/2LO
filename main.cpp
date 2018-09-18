0a083dd8f67e2a01421c078ef627841bc9305910
MicAdl <43004420+MichalAdler@users.noreply.github.com>
2018-09-18 20:35:59 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+
+cout << " Zad 3.3" << endl;
+
+cout <<3*7/2-1<< endl;
+cout <<3*7.0/2-1<< endl;
+cout <<(3*7)/2-1<< endl;
+cout <<3*(7/2.0-1)<< endl;
+cout <<3*(7/2)-1<< endl;
+cout <<3*7.0/(2-1)<< endl;
+cout <<124%12-2*4<< endl;
+cout <<4*105%10/2*108<< endl;
+cout <<(-8-13*2)/2.5<< endl;
+cout <<(pow(2.5,3)+17%5)-sqrt(4)<<endl;
+cout <<12%5+2%5<< endl;
+cout <<5+4*3.0/5<< endl;
+cout <<2%5*3-4<< endl;
+cout <<7/3%2+6<< endl;
+cout <<10-4+2/2*3<< endl;
+cout <<11/3%5+1<< endl;
+cout <<4-5/4.0*2<< endl;
+
+
+
+cout << "Zad 3.4"<< endl;
+int a,b;
+cout << sqrt(7/(pow(a,3)+cos(b)))<< endl;
+cout <<sin(pow(a+b,4)/(sqrt(11)+1))<< endl;
+cout <<pow(cos(a+1)/(sqrt(5)+3),3)<< endl;
+
+
+
+    return 0;
+}
