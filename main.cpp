2386e230ede9fd186cc38fc980dc32bc7624a4b3
MicAdl <43004420+MichalAdler@users.noreply.github.com>
2018-10-01 23:27:01 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+/*
+    int                a                   ;
+               cout << "1.6"               ;
+               cin  >> a                   ;
+    if(a%2==0) cout << "Jest parzysta"     ;
+        else   cout << "Nie jest parzysta" ;
+*/
+/*
+int  x , a ;
+                                                                              cout << "1.16" << endl  ;           ;
+                                                                              cout << "Wybierz funkcje od 1 do 5" ;
+              cin  >> a ;
+switch(a)
+{
+    case   1: cin  >> x ; if(x<1)                   cout << 2*x        ; else cout << "Nie istnieje taka funkcja" ;   break ;
+    case   2: cin  >> x ; if(x==1)                  cout << -10        ; else cout << "Nie istnieje taka funkcja" ;   break ;
+    case   3: cin  >> x ; if(x==3)                  cout << pow(x-1,4) ; else cout << "Nie istnieje taka funkcja" ;   break ;
+    case   4: cin  >> x ; if(x==6)                  cout << sqrt(x-4)  ; else cout << "Nie istnieje taka funkcja" ;   break ;
+    case   5: cin  >> x ; if(x!=1&&x!=3&&x!=6&&x>1) cout << 0          ; else cout << "Nie istnieje taka funkcja" ;   break ;
+    default :                                                                 cout << "Blad"                      ;
+
+
+
+
+}
+*/
+/*
+int a , x ;
+                                                                    cout << "1.17" << endl  ;           ;
+                                                                    cout << "Wybierz funkcje od 1-4"    ;
+              cin >> a ;
+
+switch(a)
+{
+    case   1: cin >> x ; if(x<7)       cout << (pow(x,3)+1)  ; else cout << "Nie istnieje taka funkcja" ; break ;
+    case   2: cin >> x ; if(x==7)      cout << cos(x-1)      ; else cout << "Nie istnieje taka funkcja" ; break ;
+    case   3: cin >> x ; if(x==9)      cout << sqrt(3*x)     ; else cout << "Nie istnieje taka funkcja" ; break ;
+    case   4: cin >> x ; if(x>7&&x!=9) cout << -8*x          ; else cout << "Nie istnieje taka funkcja" ; break ;
+    default :                                                       cout << "Blad"                      ;
+ }
+*/
+
+
+
+    return 0;
+}
