7f0551c256369b071f0ff6ead370ba82db4c4e60
MicAdl <43004420+MichalAdler@users.noreply.github.com>
2018-10-02 17:42:53 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+/*
+    int                a                   ;
+               cout << "1.6"               ;
+               cin  >> a                   ;
+    if(a%2==0) cout << "Jest parzysta"     ;
+        else   cout << "Nie jest parzysta" ;
+*/
+/*
+int  x  ;
+                   cout << "1.16\n"                    ;
+                   cout << "Wybierz wartosc funkcji"   ;
+cin   >> x  ;
+           if(x<1) cout << 2*x                         ;
+                else
+switch(x)
+{
+
+    case   1:      cout << -10                         ;  break ;
+    case   3:      cout << pow(x-1,4)                  ;  break ;
+    case   6:      cout << sqrt(x-4)                   ;  break ;
+    default :      cout << 0                           ;
+
+
+
+
+}
+*/
+/*
+int  x ;
+                        cout << "1.17\n"                     ;
+                        cout << "Wybierz wartosc funkcji"    ;
+    cin >> x ;
+                if(x<7) cout << (pow(x,3)+1)                 ;
+                else
+switch(x)
+{
+    case   7:           cout << cos(x-1)                     ;  break ;
+    case   9:           cout << sqrt(3*x)                    ;  break ;
+    default :           cout << -8*x                         ;
+ }
+*/
+
+    return 0;
+}
