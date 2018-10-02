6206633e54c8dba2592c0bb55747cc5a41f7c9d1
MicAdl <43004420+MichalAdler@users.noreply.github.com>
2018-10-02 22:55:06 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+
+
+/*
+int a;
+                   cout << "1.\n" ;
+    cin >> a;
+        if(a%3==0) cout << "Tak"  ;
+            else   cout << "Nie"  ;
+*/
+/*
+int a;
+                                      cout << "2.\n" ;
+    cin >> a ;
+        if(a>99 && a<1000 && a%17==0) cout << "Tak"  ;
+            else                      cout << "Nie"  ;
+*/
+/*
+int wiek;
+                    cout << "3.\n" ;
+cin >> wiek ;
+        if(wiek>17) cout << "Tak"  ;
+            else    cout << "Nie"  ;
+*/
+/*
+int waga , limit;
+                        cout << "4.\n" ;
+           limit=20                    ;
+    cin >> waga                        ;
+        if(waga<=limit) cout << "Tak"  ;
+            else        cout << "Nie"  ;
+*/
+/*
+int a , b , c ;
+                                                   cout << "5.\n" ;
+cin >> a >> b >> c ;
+        if(c<a && c>b || c>a && c<b) cout << "Tak"  ;
+            else                                   cout << "Nie"  ;
+*/
+/*
+int a , p;
+cout << "6.\n";
+cin  >> p >> a;
+    if((pow(a,p)-a)%p==0) cout << "Tak";
+        else cout << "Nie";
+*/
+
+/*
+int p , k , s      ;
+                                                 cout << "7.\n"                                                                 ;
+cin >> p >> k >> s ;
+            if(p>=0 && s>=0 && k>=0 && (3*s)>=k) cout << "Tak, przekroczyla dystans o " << (3*s)-k << "."                       ;
+                else                             cout << "Nie, brakuje jej "            << k-(3*s) << " do pokonania dystansu." ;
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
