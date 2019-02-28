1ae4af6f00eac3f429a5639bd1dc87be5d3842f9
MicAdl <43004420+MichalAdler@users.noreply.github.com>
2019-02-28 09:24:31 +0100

    Sortowania

--- main.cpp
+++ main.cpp
@@ -1,102 +1,93 @@
 #include <iostream>
-
+#include <ctime>
+#include <cstdlib>
 using namespace std;
-void wczytaj(int *T, int n)
-{
-    for(int i=0; i<n; i++)
-    {
-        cin >> T[i];
-    }
-}
-
-void szukajLidera(int *T,int n)
-{
-    int lider=T[0];
-    int pom=1;
-    int ilosc=0;
-    for(int i=0; i<n; i++)
-    {
-        if(pom==0)
-        {
-            lider=T[i];
-            pom=1;
-        }
-        else
-        {
-            if(T[i]==lider) pom++;
-                else pom--;
-        }
-    }
-    if(pom==0) cout << "Nie ma lidera";
-        else {
-            for(int i=0; i<n; i++)
-        {
-            if(lider=T[i]) ilosc++;
-        }
-        if (ilosc > n/2)
-            cout << "Lider : " << lider << endl;
-            else cout << "Nie ma lidera.";
-        }
-}
-
-bool czyRosnacy (int *T, int n)
-{
-    for(int i=0; i<n-1 ; i++)
-
-         if (T[i]>=T[i+1]) return false;
-         return true;
-
-
-
-}
-bool czyMalejacy (int *T, int n)
-{
-    for(int i=0; i<n-1 ; i++)
-
-        if (T[i]<=T[i+1]) return false;
-         return true;
-
-
-
-}
-bool czyNieRosnacy (int *T, int n)
-{
-    for(int i=0; i<n-1 ; i++)
-
-        if (T[i]<T[i+1]) return false;
-      return true;
-
-
-
-}
-bool czyNieMalejacy (int *T, int n)
-{
-    for(int i=0; i<n-1 ; i++)
-
-        if (T[i]>T[i+1]) return false;
-        return true;
-
-
-
-}
-bool czyStaly (int *T, int n)
-{
-    for(int i=0; i<n-1 ; i++)
-        if (T[i]!=T[i+1]) return false;
-
-    return true;
-}
+void wczytaj(long int *T,int n);
+void wypisz(long int *T,int n);
+void sortujBabelka(long int *T,int n);
+void sortujWybor(long int *T, int n);
+void sortZliczanie(long int *T, int n);
+int const M=100;
 int main()
 {
     int n;
     cin >> n;
-    int T[n];
+    long int T[n];
     wczytaj(T,n);
-    szukajLidera(T,n);
+    wypisz(T,n);
+    //sortujBabelka(T,n);
+    //sortujWybor(T,n);
+    sortZliczanie(T,n);
     cout << endl << endl;
-    if (czyRosnacy(T,n)) cout << "Ciag Rosnacy" << endl;
-    if (czyMalejacy(T,n)) cout << "Ciag Malejacy" << endl;
-    if (czyNieRosnacy(T,n)) cout << "Ciag Nierosnacy" << endl;
-    if (czyNieMalejacy(T,n)) cout << "Ciag Niemalejacy" << endl;
-    if (czyStaly(T,n)) cout << "Ciag Staly" << endl;
+    cout << endl << endl;
+    wypisz(T, n);
+
 }
+
+void wczytaj(long int *T,int n)
+{
+    srand(time(NULL));
+    for(int i=0; i<n; i++)
+    {
+        T[i]= rand()%M + 1;
+    }
+}
+
+void wypisz(long int *T,int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        cout << T[i] << " " <<endl;
+    }
+}
+
+void sortujBabelka(long int *T,int n)
+{
+    int i;
+   for(int j=n-1; j>0; j--)
+   {
+        for(int i=0; i<j; i++)
+        {
+        if(T[i]>T[i+1]);
+        swap(T[i],T[i+1]);
+        }
+
+   }
+
+
+}
+
+void sortujWybor(long int *T,int n)
+{
+    int k;
+   for(int i=0; i<n; i++)
+   {
+       k=i;
+        for(int j=i+1; j<n; j++)
+        {
+            if(T[j]<T[k]) k=j;
+
+        }
+            swap(T[i],T[k]);
+   }
+
+}
+
+void sortZliczanie(long int *T,int n)
+{
+    int k=0;
+    int P[M+1]={0};
+    for(int i=0; i<n; i++)
+        P[T[i]]++;
+
+    for(int i=0; i<M+1; i++)
+    {
+            for(int j=P[i]; j>0; j--)
+            {
+                T[k]=i;
+                k++;
+            }
+    }
+
+}
+
